0275245febea4d6f3c3b56fd245554f13c51d357
Sneha Chaurasia <Chaurasiasneha30@gmail.com>
2024-10-27 13:33:56 +0530

    update

---
+++ Loops.java
@@ -0,0 +1,31 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HI,Pratham");
+        }
+
+int i=0;
+while(i<10){
+    System.out.println("HI,Pratham");
+    i++;
+}
+ int x=12;
+ do {
+    System.out.println("HI,Sneha");
+    x++;
+ } while (x<10);
+ }
+
+// it execute atleatonce
+    }
+    
+
+
+
+
+
+// for loop- executed a fixed number of times-(init)
+
+//while loop-executed as long as condition is true
+
+//do while loop- executed at least once
